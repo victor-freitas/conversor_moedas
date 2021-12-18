9433896bb7dcaa0fbdfe5205dd29a63dece7f10c
victor-freitas <vitorm3rcy@gmail.com>
2021-12-18 18:24:36 -0300

    alterando arquivo html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,15 @@
 from app import app
 from flask import render_template, request
 import requests
-import json
+
 
 api_access_key = "3e8018266778321128faa8f686391525" 
 url = "http://data.fixer.io/api/latest?access_key="+api_access_key
 
 
 
-@app.route("/index2" , methods = ["GET","POST"])
-def index2():
+@app.route("/index" , methods = ["GET","POST"])
+def index():
     if request.method == "POST":
         coluna1 = request.form.get("firstCurrency")                 #'pega' o primeiro valor dado pelo usuário no HTML.
         coluna2 = request.form.get("secondCurrency")                #'pega' o segundo valor dado pelo usuário no HTML. 
@@ -19,13 +19,13 @@ def index2():
         firstValue = infos["rates"][coluna1]                        #Retorna os dados de cambio que foi solicitado no primeiro valor.
         secondValue = infos["rates"][coluna2]                       #Retorna os dados de cambio que foi solicitado no primeiro valor.
         result = (secondValue / firstValue) * float(amount)
-        currencyInfo = dict()
+        currencyInfo = dict()                                                   
         currencyInfo["coluna1"] = coluna1
         currencyInfo["secondCurrency"] = coluna2
         currencyInfo["amount"] = amount
         currencyInfo["result"] = result
         #app.logger.info(infos)
-        return render_template("index2.html",info = currencyInfo)
+        return render_template("index.html", info = currencyInfo)
     else:
-        return render_template("index2.html")   
+        return render_template("index.html")   
 
