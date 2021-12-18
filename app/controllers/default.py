e68d56c7527b30581b80b9bb675080087e459b90
victor-freitas <vitorm3rcy@gmail.com>
2021-12-18 19:52:12 -0300

    adding comments

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -3,7 +3,7 @@ from flask import render_template, request
 import requests
 
 
-api_access_key = "3e8018266778321128faa8f686391525" 
+api_access_key = "1b7ea67363e09e5c571cc1fad4ae7d77" 
 url = "http://data.fixer.io/api/latest?access_key="+api_access_key
 
 
@@ -11,21 +11,20 @@ url = "http://data.fixer.io/api/latest?access_key="+api_access_key
 @app.route("/index" , methods = ["GET","POST"])
 def index():
     if request.method == "POST":
-        coluna1 = request.form.get("firstCurrency")                 #'pega' o primeiro valor dado pelo usuário no HTML.
-        coluna2 = request.form.get("secondCurrency")                #'pega' o segundo valor dado pelo usuário no HTML. 
+        firstCurrency = request.form.get("first")                   #'pega' o primeiro valor dado pelo usuário no HTML.
+        secondCurrency = request.form.get("second")                 #'pega' o segundo valor dado pelo usuário no HTML. 
         amount = request.form.get("amount")                         #'pega' o valor digitado pelo usuário.
         response = requests.get(url)                                #faz a requisição da URL que foi passada --> http://data.fixer.io/api/latest?access_key="
-        infos = response.json()                                     #pega o fluxo de respostas da API e mostra o resultado do texto como um JSON.
-        firstValue = infos["rates"][coluna1]                        #Retorna os dados de cambio que foi solicitado no primeiro valor.
-        secondValue = infos["rates"][coluna2]                       #Retorna os dados de cambio que foi solicitado no primeiro valor.
-        result = (secondValue / firstValue) * float(amount)
-        currencyInfo = dict()                                                   
-        currencyInfo["coluna1"] = coluna1
-        currencyInfo["secondCurrency"] = coluna2
-        currencyInfo["amount"] = amount
-        currencyInfo["result"] = result
-        #app.logger.info(infos)
-        return render_template("index.html", info = currencyInfo)
+        info = response.json()                                      #pega o fluxo de respostas da API e mostra o resultado do texto como um JSON.
+        firstValue = info["rates"][firstCurrency]                   #[rates] Retorna dados de taxa de câmbio para as moedas que você solicitou.
+        secondValue = info["rates"][secondCurrency]                      
+        result = (secondValue / firstValue) * float(amount)         #variavel result recebe o segundo valor e divide pelo primeiro e multiplicando pelo valor.
+        currencyInfo = dict()                                       #funcao que cria um dicionario           
+        currencyInfo["first"]  = firstCurrency                      #Atribui o primeiro escolhido do usuario com o select cujo nome é first
+        currencyInfo["second"] = secondCurrency                     #Atribui o segundo escolhido do usuario com o select cujo nome é second
+        currencyInfo["amount"] = amount                             #Armazena o valor escolhido pelo usuario e atribui com input cuja class é amount         
+        currencyInfo["result"] = result                             #Armazena o valor do resultado              
+        return render_template("index.html", info = currencyInfo)   #Imprime na mesma tela do index a taxa de cambio com o metodo info
     else:
         return render_template("index.html")   
 
